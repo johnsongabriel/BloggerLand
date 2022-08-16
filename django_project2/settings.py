2be9c98d210cf72f906b388cd290c1b193ea8561
Johnson-2003 <johnsongabimoisili2003@gmail.com>
2022-08-16 19:52:56 +0100

    heroku confuguration correction

--- django_project2/settings.py
+++ django_project2/settings.py
@@ -10,8 +10,10 @@ For the full list of settings and their values, see
 https://docs.djangoproject.com/en/4.0/ref/settings/
 """
 
+
 import os
 from pathlib import Path
+
 
 # Build paths inside the project like this: BASE_DIR / 'subdir'.
 BASE_DIR = Path(__file__).resolve().parent.parent
@@ -145,3 +147,4 @@ EMAIL_HOST_PASSWORD = 'itmbavtbfkpfbmuq'
 # https://docs.djangoproject.com/en/4.0/ref/settings/#default-auto-field
 
 DEFAULT_AUTO_FIELD = 'django.db.models.BigAutoField'
+
