79fce1b6d53dbf3105b992e689ce5717f7d3704d
Johnson-2003 <johnsongabimoisili2003@gmail.com>
2022-08-20 10:43:57 +0100

    added static root and collectstatic

--- django_project2/settings.py
+++ django_project2/settings.py
@@ -126,8 +126,7 @@ STATIC_URL = 'static/'
 STATICFILES_DIR = [
     BASE_DIR / "static"
 ]
-
-MEDIA_ROOT = os.path.join(BASE_DIR, 'media')
+STATIC_ROOT = os.path.join(BASE_DIR, 'media')
 MEDIA_URL = '/media/'
 
 CRISPY_TEMPLATE_PACK = 'bootstrap4'
