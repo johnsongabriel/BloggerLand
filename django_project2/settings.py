f4d4a22e93599943c133f525515b809378bffe68
Johnson-2003 <johnsongabimoisili2003@gmail.com>
2022-08-20 15:31:01 +0100

    adding static root

--- django_project2/settings.py
+++ django_project2/settings.py
@@ -122,11 +122,11 @@ USE_TZ = True
 # https://docs.djangoproject.com/en/4.0/howto/static-files/
 
 STATIC_URL = 'static/'
-
+STATIC_ROOT = os.path.join(BASE_DIR, 'static')
 STATICFILES_DIR = [
     BASE_DIR / "static"
 ]
-STATIC_ROOT = os.path.join(BASE_DIR, 'media')
+MEDIA_ROOT = os.path.join(BASE_DIR, 'media')
 MEDIA_URL = '/media/'
 
 CRISPY_TEMPLATE_PACK = 'bootstrap4'
